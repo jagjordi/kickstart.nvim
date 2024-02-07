eba6e0158b80b66d99f50e2e5b3c6ce00ef55030
Jordi Altayo <jordi@s2683.ict.kth.se>
2024-02-07 20:51:17 +0100

    work

--- lua/custom/plugins/latex.lua
+++ lua/custom/plugins/latex.lua
@@ -1,3 +1,7 @@
 return {
-  "lervag/vimtex"
+  "lervag/vimtex",
+  config = function ()
+    vim.g.vimtex_view_method = 'sioyek'
+    vim.g.vimtex_view_sioyek_exe = '/var/lib/flatpak/exports/bin/com.github.ahrm.sioyek'
+  end
 }
